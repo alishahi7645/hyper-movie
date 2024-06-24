03ccc69519b529f4c538a828b425b67034f2fcaf
alishahi <shahy8227@gmail.com>
2024-06-25 01:53:31 +0330

    start designed headerslider

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Navigation from './Navigation'
-import Searchbox from './Searchbox'
-import FollowUs from './FollowUs'
+import React from "react";
+import Navigation from "./Navigation";
+import Searchbox from "./Searchbox";
+import FollowUs from "./FollowUs";
+import Slider from "./Slider";
 
 function Header() {
   return (
     <header>
-      <Navigation/>
-      <Searchbox/>
-      <FollowUs/>
+      <div className="containe">
+        <Navigation />
+        <Searchbox />
+        <FollowUs />
+        <Slider />
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
