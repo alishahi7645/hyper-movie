5dfcfefab89423e61ae50a507e24109304444ed3
alishahi <shahy8227@gmail.com>
2024-07-03 17:14:36 +0330

    start designed hover background

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,11 @@ import Navigation from "./Navigation";
 import Searchbox from "./Searchbox";
 import FollowUs from "./FollowUs";
 import Slider from "./Slider";
-import bg from '../../assets/images/cinema.jpg'
+
 
 function Header() {
   return (
-    <header className="py-8" style={{backgroundImage: `linear-gradient(to bottom, rgb(0 0 0 / 90%), rgb(0 0 0 / 70%), rgb(0 0 0 / 40%)), url(${bg})`}}>
+    <header className="py-8" style={{backgroundImage: `linear-gradient(to bottom, rgb(0 0 0 / 90%), rgb(0 0 0 / 70%), rgb(0 0 0 / 40%)), url('/cinema.jpg')`}}>
       <div className="container">
         <Navigation />
         <Searchbox />
