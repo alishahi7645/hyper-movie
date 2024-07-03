15bc74815b0460ff329c34a6ceee8c75915ab156
alishahi <shahy8227@gmail.com>
2024-07-03 17:42:53 +0330

    end designed hoved vackground image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation";
 import Searchbox from "./Searchbox";
 import FollowUs from "./FollowUs";
@@ -6,13 +6,14 @@ import Slider from "./Slider";
 
 
 function Header() {
+  const [bg , setbg] = useState('/cinema.jpg')
   return (
-    <header className="py-8" style={{backgroundImage: `linear-gradient(to bottom, rgb(0 0 0 / 90%), rgb(0 0 0 / 70%), rgb(0 0 0 / 40%)), url('/cinema.jpg')`}}>
+    <header className="py-8 transition duration-700 bg-contain bg-center" style={{backgroundImage: `linear-gradient(to bottom, rgb(0 0 0 / 90%), rgb(0 0 0 / 70%), rgb(0 0 0 / 40%)), url('${bg}')`}}>
       <div className="container">
         <Navigation />
         <Searchbox />
         <FollowUs />
-        <Slider />
+        <Slider setbg={setbg}/>
       </div>
     </header>
   );
--- src/components/header/Slider.jsx
+++ src/components/header/Slider.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
-function Slider() {
+function Slider({setbg}) {
+  function getImage(number){
+    return `http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider${number}.jpg`
+  }
   return (
     <div className="mt-8 mb-12">
       <Swiper
@@ -16,8 +19,9 @@ function Slider() {
           [1,2,3,4].map((number) => (
             <SwiperSlide key={number}>
               <img
-              className="w-full"
-              src={`http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider${number}.jpg`} alt="" />
+              onMouseOver={(e) => setbg(getImage(number))}
+              className="w-full rounded"
+              src={getImage(number)} alt="" />
             </SwiperSlide>
           ))
         }
