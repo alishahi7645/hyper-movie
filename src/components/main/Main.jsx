735a88072060012960ca7b8664077e2e2cefb6c9
alishahi <shahy8227@gmail.com>
2024-07-06 19:37:16 +0330

    start designed movies list

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MoviesList from './MoviesList'
+
+function Main() {
+  return (
+    <MoviesList/>
+  )
+}
+
+export default Main
---
+++ src/components/movies/MoviesCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MoviesCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MoviesCard
