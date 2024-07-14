ffdf844586f51c3230e3d54dc02dc90999690e4e
alishahi <shahy8227@gmail.com>
2024-07-15 03:28:02 +0330

    added footer demo

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-center bg-slate-900 py-6 mt-8'>
+        Footer
+    </footer>
+  )
+}
+
+export default Footer
