54addbe31ba21c2c0284312e512bd417c6491368
alishahi <shahy8227@gmail.com>
2024-07-15 03:21:40 +0330

    added filters section Whats popular

--- src/components/main/MovieListSlider.jsx
+++ src/components/main/MovieListSlider.jsx
@@ -1,46 +1,47 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
+import MoviesCard from "../movies/MoviesCard";
 
-function MovieListSlider() {
+function MovieListSlider({ movies }) {
   function getImage(number) {
     return `http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider${number}.jpg`;
   }
   return (
-    <div className="mt-8 mb-12">
-      <Swiper
-        breakpoints={{
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-        }}
-        spaceBetween={20}
-        slidesPerView={1}
-        modules={[Autoplay]}
-        autoplay={{ delay: 2000 }}
-        loop
-      >
-        {[1, 2, 3, 4].map((number) => (
-          <SwiperSlide key={number}>
-            <img
+    <Swiper
+      breakpoints={{
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 5,
+        },
+      }}
+      spaceBetween={20}
+      slidesPerView={1}
+      modules={[Autoplay]}
+      autoplay={{ delay: 3000 }}
+      centeredSlides
+      loop
+    >
+      {movies.map((img) => (
+        <SwiperSlide key={img}>
+          {/* <img
               onMouseOver={(e) => setbg(getImage(number))}
               className="w-full rounded"
               src={getImage(number)}
               alt=""
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+            /> */}
+          <MoviesCard img={img}/>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
 
--- src/components/main/MoviesList.jsx
+++ src/components/main/MoviesList.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import MoviesCard from "../movies/MoviesCard";
+import MovieListSlider from "./MovieListSlider";
+
+const movies = [
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider1.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider2.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider3.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider4.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider1.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider2.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider3.jpg',
+  'http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider2.jpg',
+]
 
 function MoviesList() {
   return (
     <div className="container">
-      <div className="py-8 columns-6">
-        <MoviesCard img='http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider1.jpg' />
-        <MoviesCard img='http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider2.jpg' />
-        <MoviesCard img='http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider3.jpg'/>
-        <MoviesCard img='http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider4.jpg'/>
-        <MoviesCard img='http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider4.jpg'/>
-        <MoviesCard img='http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider4.jpg'/>
+      <div className="pt-8">
+        <div className="md:flex gap-8 items-center md:mb-4">
+          <h2 className="text-slate300 text-2xl">What`s Popular</h2>
+          <ul className="my-6 flex flex-col gap-2 md:flex-row">
+            <li className="text-yellow-200">#Streaming</li>
+            <li>#On Tv</li>
+            <li>#For Rent</li>
+            <li>#In Theaters</li>
+          </ul>
+        </div>
+        <MovieListSlider movies={movies}/>
       </div>
+      
     </div>
   );
 }
