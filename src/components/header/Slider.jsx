8d319eb9bf4cf2e47882bdd022d24d3ef6ff886e
alishahi <shahy8227@gmail.com>
2024-07-19 23:34:53 +0330

    slider complate

--- src/components/header/Slider.jsx
+++ src/components/header/Slider.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import MoviesCard from "../movies/MoviesCard";
+import axios from "axios";
 
 function Slider({setbg}) {
 
   const [movies , setMovies] = useState([])
+
+  async function loadMovies() {
+    const {data} = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=2d65b06dcf682524c5198a666426664c')
+
+    setMovies(data.results)
+  }
+
+  useEffect(()=>{
+    loadMovies();
+  },[])
 
   function getImage(number){
     return `http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider${number}.jpg`
@@ -34,13 +45,14 @@ function Slider({setbg}) {
         loop
       >
         {
-          [1,2,3,4].map((number) => (
-            <SwiperSlide key={number}>
-              <div  onMouseOver={(e) => setbg(getImage(number))}>
-              <MoviesCard img={`http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider${number}.jpg`}/>
+          movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <div  onMouseOver={(e) => setbg(`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`)}>
+                <MoviesCard title={movie.title} rate={movie.vote_average} img={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}/>
               </div>
             </SwiperSlide>
           ))
+          
         }
       </Swiper>
     </div>
--- src/components/movies/MoviesCard.jsx
+++ src/components/movies/MoviesCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function MoviesCard({img}) {
+function MoviesCard({img , title , rate}) {
   return (
     <div className="bg-gray-300 aspect-[2/3] relative rounded overflow-hidden">
       <img
@@ -9,7 +9,7 @@ function MoviesCard({img}) {
         alt=""
       />
       <div className="flex flex-col justify-end p-8 absolute bottom-0 w-full h-full bg-gradient-to-b from-[#0f172a12] to-[#0f172a81] hover:from-[#0f172a49] hover:to-[#0f172ac3]">
-        <h3>Die Hard</h3>
+        <h3>{title}</h3>
         <div className="flex gap-2 mt-">
           <span className="text-yellow-400">
             <svg
@@ -23,7 +23,7 @@ function MoviesCard({img}) {
               <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
             </svg>
           </span>
-          <span>7.4 / 10</span>
+          <span>{rate} / 10</span>
         </div>
       </div>
     </div>
