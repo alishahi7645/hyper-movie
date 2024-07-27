4f3800c8bc3a832718c6dfeff32c54b521584215
alishahi <shahy8227@gmail.com>
2024-07-27 15:32:15 +0330

    Show the list of movies only on the main page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,17 +3,28 @@ import Navigation from "./Navigation";
 import Searchbox from "./Searchbox";
 import FollowUs from "./FollowUs";
 import Slider from "./Slider";
-
+import { useLocation } from "react-router-dom";
 
 function Header() {
-  const [bg , setbg] = useState('/cinema.jpg')
+  const [bg, setbg] = useState("/cinema.jpg");
+
+  const locaition = useLocation();
   return (
-    <header className="py-4 md:py-8 transition duration-700 bg-contain bg-center" style={{backgroundImage: `linear-gradient(to bottom, rgb(0 0 0 / 90%), rgb(0 0 0 / 70%), rgb(0 0 0 / 40%)), url('${bg}')`}}>
+    <header
+      className={`py-4 md:py-8 transition duration-700 bg-contain bg-center ${
+        locaition.pathname != "/" ? "h-[500px]" : ""
+      }`}
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgb(0 0 0 / 90%), rgb(0 0 0 / 70%), rgb(0 0 0 / 40%)), url('${bg}')`,
+      }}
+    >
       <div className="container">
         <Navigation />
         <Searchbox />
-        <FollowUs />
-        <Slider setbg={setbg}/>
+        <div className={`${locaition.pathname != "/" ? "hidden" : ""}`}>
+          <FollowUs />
+          <Slider setbg={setbg} />
+        </div>
       </div>
     </header>
   );
